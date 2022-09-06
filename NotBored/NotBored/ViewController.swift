45579d26bf2162097065f68d5fb865756fe5ac13
Danielle <danielle.ogawa@mercadolivre.com>
2022-09-06 09:03:55 -0300

    criando projeto xcode

---
+++ NotBored/NotBored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotBored
+//
+//  Created by Danielle Nozaki Ogawa on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
